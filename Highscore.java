fdb6ff3f318f6a6549690db72f0cad48b707cbdd
Shay <154104485+ShayArgaman@users.noreply.github.com>
2024-09-03 20:48:09 +0300

    Update Highscore.java

--- Highscore.java
+++ Highscore.java
@@ -1,66 +1,32 @@
 package SnakeGame;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.Scanner;
+import java.io.*;
 
 public class Highscore {
-	private int score;
-	
-	public Highscore() {
-		this.score=0;
-	}
-	
-	public int readHighScore() throws FileNotFoundException{
+    private String filePath;
 
-		try {
-			Scanner readScore = new Scanner(new File("C:\\Users\\User\\eclipse-workspace\\firstyear\\MyProject\\SnakeGame\\highscore.txt"));
-			while(readScore.hasNextLine()) {
-				int oldHighScore = readScore.nextInt();
-				if(this.score<oldHighScore) {
-					this.score=oldHighScore;
-				}
-			}
-			readScore.close();
-		} catch(FileNotFoundException e) {
-			e.printStackTrace();
-		}
-		return this.score;
-	}
-	
-	public void updateHighScore(int score) {
-	    // Only update the high score if the new score is greater
-	    if (score > this.score) {
-	        try {
-	            BufferedWriter writeScore = new BufferedWriter(new FileWriter("C:\\Users\\User\\eclipse-workspace\\firstyear\\MyProject\\SnakeGame\\highscore.txt"));
-	            writeScore.write(String.valueOf(score)); // Write the new high score
-	            writeScore.close();
-	        } catch (IOException e) {
-	            e.printStackTrace();
-	        }
-	    }
-	}
+    public Highscore(String filePath) {
+        this.filePath = filePath;
+    }
 
+    public int readHighScore() {
+        int highscore = 0;
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line = reader.readLine();
+            if (line != null) {
+                highscore = Integer.parseInt(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return highscore;
+    }
+
+    public void saveHighScore(int score) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
+            writer.write(String.valueOf(score));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
-
-//public void updateHighScore(int newScore) {
-//    if (score < newScore) {
-//        score = newScore;
-//        try {
-//            // Update the high score file
-//            java.nio.file.Files.write(new File("highscore.txt").toPath(), String.valueOf(newScore).getBytes());
-//        } catch (Exception e) {
-//            System.out.println("Error updating high score: " + e.getMessage());
-//        }
-//    }
-//}
-//
-//public int getScore() {
-//    return score;
-//}
-//
-//}
-
